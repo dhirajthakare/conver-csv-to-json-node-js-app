754e3500bda9fa70a9f2d58cfd4e444e382355c0
dhiraj_thakare_office <88828651+DhirajLucidux@users.noreply.github.com>
2023-03-31 22:46:15 +0530

    updated code make custome csv to pdf converter

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const csvFileToJson = require("csvtojson");
 const { Postgres } = require("pg");
 require("dotenv").config(".env");
 
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
 const app = express();
 
 app.get("/csvtojson", async (req, res) => {
@@ -17,10 +20,64 @@ app.get("/csvtojson", async (req, res) => {
 
     res.send(mydata);
   } catch (err) {
-    console.error(err);
     res.status(500).send("Internal server error");
   }
 });
+
+let labels;
+let records = [];
+let sendResponce = [];
+let newarr = {};
+
+app.get("/csvtojsoncustomeparser", async (req, res) => {
+  labels;
+  records = [];
+  sendResponce = [];
+
+  fs.createReadStream("csvSheet.csv")
+    .pipe(parse({ delimiter: "," }))
+    .on("data", function (row) {
+      records.push(row);
+    });
+  setTimeout(() => {
+    let responce = claculatejson(records);
+    res.json(responce);
+  }, 1000);
+});
+
+function claculatejson(data) {
+  let firstRow = data[0];
+
+  for (let index = 1; index < data.length; index++) {
+    labels = convertJsonArray(firstRow, data[index]);
+    sendResponce.push(labels);
+  }
+
+  return sendResponce;
+}
+
+function convertJsonArray(labels, currentData) {
+  newarr = {};
+
+  for (let index = 0; index < labels.length; index++) {
+    split = labels[index].split(".");
+    makeDynamicObj(newarr, split, currentData[index]);
+  }
+
+  return newarr;
+}
+
+function makeDynamicObj(curentarr, keyPath, value) {
+  lastKeyIndex = keyPath.length - 1;
+  for (var i = 0; i < lastKeyIndex; ++i) {
+    key = keyPath[i];
+    if (!(key in curentarr)) {
+      curentarr[key] = {};
+    }
+    curentarr = curentarr[key];
+  }
+  curentarr[keyPath[lastKeyIndex]] = value;
+}
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
