a1dc7c54e31e55089b08324d574e1206a9a334c3
dhiraj_thakare_office <88828651+DhirajLucidux@users.noreply.github.com>
2023-03-31 17:47:26 +0530

    updated code for simple csv to json converter

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# csv File to Json Coverter
+
+### csv file 
+
+
+![image](https://user-images.githubusercontent.com/32159491/229116653-253b1ca0-45b5-4c05-8ac8-fadefc66f91e.png)
+
+
+### out of this csv file in json
+
+
+![image](https://user-images.githubusercontent.com/32159491/229116321-8f2443e6-8efb-4599-b46a-72a5b2c3518f.png)
--- server.js
+++ server.js
@@ -5,56 +5,17 @@ require("dotenv").config(".env");
 
 const app = express();
 
-const Postgres = new Postgres({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
-
-app.post("/uploadecsv", async (req, res) => {
+app.get("/csvtojson", async (req, res) => {
   try {
-    const convertedJsonArray = await csvFileToJson().fromFile(
-      "process.env.CSV_FILE_PATH"
-    );
-    // console.log(convertedJsonArray);
-    const connection = await Postgres.connect();
-    await connection.query("BEGIN");
-    const insertQuery =
-      "INSERT INTO users(name, age, address, additional_info) VALUES($1, $2, $3, $4) RETURNING id";
-    for (let i = 0; i < convertedJsonArray.length; i++) {
-      const { firstName, lastName, age, ...remaining } = convertedJsonArray[i];
-      const address = {
-        line1: remaining["address.line1"],
-        line2: remaining["address.line2"],
-        city: remaining["address.city"],
-        state: remaining["address.state"],
-      };
-      const additionalInfo = { ...remaining };
-      delete additionalInfo["address.line1"];
-      delete additionalInfo["address.line2"];
-      delete additionalInfo["address.city"];
-      delete additionalInfo["address.state"];
-      const { records } = await connection.query(insertQuery, [
-        `${firstName} ${lastName}`,
-        parseInt(age),
-        address,
-        additionalInfo,
-      ]);
-      console.log(`Inserted user with ID ${records[0].id}`);
-    }
-    await connection.query("COMMIT");
-    res.send("row Added successfully");
+    const convertedJsonArray = await csvFileToJson().fromFile("csvSheet.csv");
 
-    const query =
-      "SELECT COUNT(*) as count, age FROM users GROUP BY age ORDER BY age";
-    const { records } = await connection.query(query);
-    console.log(records);
-    console.log("Age distribution of Users:");
-    console.log("Age\tCount");
-    records.forEach((item) => console.log(`${item.age}\t${item.count}`));
-    res.json(records);
+    let mydata = [];
+
+    for (let i = 0; i < convertedJsonArray.length; i++) {
+      mydata.push(convertedJsonArray[i]);
+    }
+
+    res.send(mydata);
   } catch (err) {
     console.error(err);
     res.status(500).send("Internal server error");
